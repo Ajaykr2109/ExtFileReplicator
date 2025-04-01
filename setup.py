d16a6dd99aee7a5b5702c294d6bbf62b6ee5d92e
ajay <chaturvedikraj2109@gmail.com>
2025-04-01 05:43:15 +0530

    Setup.py restocked removed setup.cfg for now from setp.py will see what we can do ahead

--- setup.py
+++ setup.py
@@ -1,5 +1,29 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-if __name__ == "__main__":
-    setup()
+setup(
+    name="ext_folder_replicator",
+    version="1.0.0",
+    author="ajaykr2109_nowStack",
+    author_email="chaturvedikraj2109@gmail.com",
+    description="A CLI-based folder replication and synchronization tool.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ajaykr2109/ExtFileReplicator",
+    packages=find_packages(),
+    install_requires=[
+        "watchdog",
+        "pywin32; sys_platform == 'win32'",
+    ],
+    entry_points={
+        "console_scripts": [
+            "frep=folder_replicator.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    include_package_data=True,
+)
